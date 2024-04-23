d95fe5ad8180a0617a182e0e0b3a6e51af70e154
WFaguirre <fe.aguirre.agudelo@gmail.com>
2024-04-22 23:49:42 -0500

    Workshop3 finish with you respective code and instrutions

---
+++ seccion2/app.js
@@ -0,0 +1,75 @@
+/* //Espero aprender los conceptos más utilizados en javascript para aplicarlo a mis paginas web
+
+/**Espero aprender lo que más pueda
+ * con los seiguientes ejercicios de 
+ * Javascript
+ */
+
+// //seccion2
+// //Ejercicio 5
+let miNumero = 10;
+console.log(miNumero);
+// //Ejercicio 6
+let miBooleano = true;
+console.log(typeof miBooleano);
+// //Ejercicio 7
+let miCadena= 'esternomaiclodeo';
+
+console.log(miCadena.length);
+
+// //Ejercicio 8
+let miArray = ['perrito', 16, ['granja', 'campesino', 'gallina']];
+
+console.log(miArray);
+
+// //Ejercicio 9
+miObjeto = {
+
+    id : 115226988,
+    nombre : 'Armando Baretos',
+    profesion : 'mecanico'
+
+}
+
+console.log(miObjeto);
+
+// //Seccion 3
+// //Ejercicio 10
+
+let nombreUsuario = prompt('Escriba su nombre: ');
+
+window.alert('Hola, bienvenido: ' + nombreUsuario);
+
+// // Ejercicio 11
+let desition = confirm("Te gusta Javascript 'Aceptar/si' 'Cancelar/no': ");
+
+if (desition == true){
+    console.log('SI')
+} else {
+    console.log('NO')
+
+}
+
+// Seccion 4
+// Ejercicio 9
+
+console.warn('Esto es solo una practica');  
+
+// Ejercicio 10
+
+console.error('Has entrado a algo indebido')
+
+
+// Ejercicio  11
+
+const array = ["1","2","3","4","5"];
+
+array.forEach((elemento) => console.table(elemento));
+
+
+
+
+
+
+
+
